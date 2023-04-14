60e0d2d52e4818b65bac7967493579bf0d5de614
iiscremona-admin <github@iiscremona.it>
2023-04-14 13:12:06 +0200

    Update base-os.ino

--- arduino/base-os/base-os.ino
+++ arduino/base-os/base-os.ino
@@ -15,8 +15,8 @@ LICENSE: GPL-3.0
 #include <nRF24L01.h>
 #include <RF24.h>
 
-#include "secret.h"//WiFi Password and API Key
-#include "constant.h"//Constanti non importanti per la sicurezza
+#include "secret.h"    //WiFi Password and API Key
+#include "constant.h"  //Constanti non importanti per la sicurezza
 
 /*
 Costanti presenti nei file ".h":
@@ -28,8 +28,13 @@ WIFI_PASSWORD: Password della WiFi
 
 */
 
+#if CONFIG_FREERTOS_UNICORE
+#define ARDUINO_RUNNING_CORE 0
+#else
+#define ARDUINO_RUNNING_CORE 1
+#endif
 
-#define ARDUINOJSON_USE_DOUBLE 0 //arduinojson setting
+#define ARDUINOJSON_USE_DOUBLE 0  //arduinojson setting
 
 String key = KEY;
 
@@ -37,9 +42,13 @@ const char *rootSSLCertificate = SSL_CERTIFICATE;
 
 String api_server_url = "https://" + API_SERVER
 
-QueueHandle_t xDatasQueue; //queue usata per passare i dati attraverso i task
+                          QueueHandle_t xDatasQueue;  //queue usata per passare i dati attraverso i task
 
 WiFiMulti WiFiMulti;
+
+RF24 radio;  // CE, CSN
+const byte address[6] = "00001";
+
 
 //led indicativi CORE0
 #define LED_CORE0a 32
@@ -51,29 +60,81 @@ WiFiMulti WiFiMulti;
 #define LED_CORE1b 35
 
 
+//CE e CSN per la comunicazione radio
 
-//INPUT TASK 
+#define CE 26
+#define CSN 25
+
+
+
+//INPUT TASK
 //questi task devono girare sul core 0
 
-void radioTask(void *pvParameters){
-  //Controlla constantemente se arrivano dati dal modulo radio e li inserisce nella queue
-}
+void radioTask(void *pvParameters);
+//Controlla constantemente se arrivano dati dal modulo radio e li inserisce nella queue
+
 
 
 //OUTPUT TASK
 //questi task devono girare sul core 1
-void uploadTask(void *pvParameters){
-  //Prende i dati dalla queue e li li inserisce sul DB con una richiesta alle API
-  
-}
+void uploadTask(void *pvParameters);
+//Prende i dati dalla queue e li li inserisce sul DB con una richiesta alle API
 
-void sdcardTask(void *pvParameters) {
-  //TO DO in future
-  //Prende i dati dalla queue e li salva su una scheda SD
-}
+
+
+void sdcardTask(void *pvParameters);
+//TO DO in future
+//Prende i dati dalla queue e li salva su una scheda SD
+
 
 
 void setup() {
   Serial.begin(115200);
 
+  if (!radio.begin(CE, CSN)) {
+    Serial.println("La radio non funziona");
+    while (1) { Serial.println("La radio non funziona"); }  // hold program in infinite loop to prevent subsequent errors
+  }
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_HIGH);
+  radio.startListening();
+
+
+  xQueueDatas = xQueueCreate(6, 10);
+
+  xTaskCreatePinnedToCore(
+    radioTask, "radioTask"  // A name just for humans
+    ,
+    1024 * 10  // This stack size can be checked & adjusted by reading the Stack Highwater
+    ,
+    NULL, 200  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,
+    NULL, 0);
+
+  xTaskCreatePinnedToCore(
+    uploadTask, "uploadTask", 1024 * 64  // Stack size
+    ,
+    NULL, 200  // Priority
+    ,
+    NULL, 1);
+}
+
+void loop() {
+}
+
+void radioTask(void *pvParameters) {
+
+  (void)pvParameters;
+
+  for ( ; ; ) {
+    Serial.println("Ascoltando sul canale radio...");
+   if (radio.available()) {
+    char data[32] = "";
+    radio.read(&data, sizeof(data));
+    Serial.println(data);
+    
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+   }
+    vTaskDelay(5 / portTICK_PERIOD_MS);
+  }
 }
