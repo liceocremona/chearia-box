9b34780f3f770622eadf9ec5c39068d8eb1b93ad
Nicolò Valenza <nico.vale.spazio@gmail.com>
2023-04-25 00:43:16 +0200

    Add comments and fix little bug

--- arduino/base-os/base-os.ino
+++ arduino/base-os/base-os.ino
@@ -28,13 +28,11 @@ WIFI_PASSWORD: Password della WiFi
 
 */
 
+#define ARDUINOJSON_USE_DOUBLE 0  // arduinojson setting
 
+String key = KEY;  // Chiave autenticazione API
 
-#define ARDUINOJSON_USE_DOUBLE 0  //arduinojson setting
-
-String key = KEY;
-
-const char *rootCACertificate =
+const char *rootCACertificate =  // certificato publico ssl
   "-----BEGIN CERTIFICATE-----\n"
   \ 
 "MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n"
@@ -96,51 +94,33 @@ const char *rootCACertificate =
 "emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n"
   \ 
 "-----END CERTIFICATE-----\n";
+
 String api_server = API_SERVER;
+String api_server_url = String("https://" + api_server);  // URl del server delle API
 
-String api_server_url = String("https://" + api_server);
+QueueHandle_t xQueueDatas;  // queue usata per passare i dati attraverso i task
 
-QueueHandle_t xQueueDatas;  //queue usata per passare i dati attraverso i task
+WiFiMulti WiFiMulti;  // oggetto per la gestione delle connessioni wifi
 
-WiFiMulti WiFiMulti;
+RF24 radio;                       // modulo radio
+const byte address[6] = "00001";  // indirizzo del canale radio usato per la comunicazione
 
-RF24 radio;  // CE, CSN
-const byte address[6] = "00001";
-
-
-//led indicativi CORE0
+// led info CORE0
 #define LED_CORE0a 32
 #define LED_CORE0b 33
 
-
-//led indicativi CORE1
+// led info CORE1
 #define LED_CORE1a 34
 #define LED_CORE1b 35
 
-
-//CE e CSN per la comunicazione radio
-
+// CE e CSN del modulo Nrf24l01 per la radio
 #define CE 4
 #define CSN 5
-
-
-
-//INPUT TASK
-//questi task devono girare sul core 0
 
 bool wifi_conn = false;
 bool radio_init = false;
 
-void radioTask(void *pvParameters);
-//Controlla constantemente se arrivano dati dal modulo radio e li inserisce nella queue
-
-
-
-//OUTPUT TASK
-//questi task devono girare sul core 1
-void uploadTask(void *pvParameters);
-//Prende i dati dalla queue e li li inserisce sul DB con una richiesta alle API
-
+// Struttura dati per i dati ricevuti dal modulo radio
 struct receiveDataStruct {
   String data_name;
   float data_value;
@@ -148,148 +128,152 @@ struct receiveDataStruct {
 
 receiveDataStruct parseData(String data) {
   receiveDataStruct data_structure;
-  //Qesta funzione ottiene la String dalla funzione precedente e separa il valore dal nome del dato
-  //Es. 22.8-temperature --> valore: 22.8 nome dato: temperature
-  int char_len = data.length() + 1;                 //Lunghezza String
-  char coded_data[char_len];                        //Creazione di un array di char pari alla lunghezza della String
-  data.toCharArray(coded_data, char_len);           //Conversione della Strin in un array di char
-  char *ch_measurement = strtok(coded_data, "-");   //Array di char contenente il valore
-  char *data_name_ch = strtok(NULL, "-");           //Array di char contenente il nome del dato
-  String str_measurement = String(ch_measurement);  //COnversione in Float
+  // Qesta funzione ottiene la String dalla funzione precedente e separa il valore dal nome del dato
+  // Es. 22.8-temperature --> valore: 22.8 nome dato: temperature
+  int char_len = data.length() + 1;                 // Lunghezza String
+  char coded_data[char_len];                        // Creazione di un array di char pari alla lunghezza della String
+  data.toCharArray(coded_data, char_len);           // Conversione della Strin in un array di char
+  char *ch_measurement = strtok(coded_data, "-");   // Array di char contenente il valore
+  char *data_name_ch = strtok(NULL, "-");           // Array di char contenente il nome del dato
+  String str_measurement = String(ch_measurement);  // COnversione in Float
   float measurement = str_measurement.toFloat();
   String data_name = String(data_name_ch);
-  data_structure.data_name = data_name;
+  data_structure.data_name = data_name;  // Inserimento dei dati nella struct
   data_structure.data_value = measurement;
-  return data_structure;
+  return data_structure;  // Ritorno della struct
 }
 
+// INPUT TASK
+// questi task devono girare sul core 0
+
+void radioTask(void *pvParameters);
+// Controlla constantemente se arrivano dati dal modulo radio e li inserisce nella queue
+
+// OUTPUT TASK
+// questi task devono girare sul core 1
+void uploadTask(void *pvParameters);
+// Prende i dati dalla queue e li li inserisce sul DB con una richiesta alle API
+
 void sdcardTask(void *pvParameters);
-//TO DO in future
-//Prende i dati dalla queue e li salva su una scheda SD
-
-
+// TO DO in future
+// Prende i dati dalla queue e li salva su una scheda SD
 
 void setup() {
   Serial.begin(115200);
 
-  while (!wifi_conn) {
+  while (!wifi_conn) {  // connessione alla rete WiFi
     WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+    WiFiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
 
-  // wait for WiFi connection
-  Serial.print("Waiting for WiFi to connect...");
-  while ((WiFiMulti.run() != WL_CONNECTED)) {
-    Serial.print(".");
+    // wait for WiFi connection
+    Serial.print("Waiting for WiFi to connect...");
+    while ((WiFiMulti.run() != WL_CONNECTED)) {
+      Serial.print(".");
+    }
+    Serial.println(" connected");
+    wifi_conn = true;
   }
-  Serial.println(" connected");
-  wifi_conn = true;
 
+  if (!radio_init) {              // inizializzazione del modulo radio
+    if (!radio.begin(CE, CSN)) {  // CE, CSN
+      Serial.println("La radio non funziona");
+      while (1) {
+        Serial.println("La radio non funziona");
+      }  // hold program in infinite loop to prevent subsequent errors
+    }
+    radio.openReadingPipe(0, address);
+    radio.setPALevel(RF24_PA_HIGH);
+    radio.startListening();
+    radio_init = true;
   }
 
-   if (!radio_init) {
- if (!radio.begin(CE, CSN)) {
-    Serial.println("La radio non funziona");
-    while (1) { Serial.println("La radio non funziona"); }  // hold program in infinite loop to prevent subsequent errors
-  }
-  radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_HIGH);
-  radio.startListening();
-  radio_init = true;    
-  }
- 
+  xQueueDatas = xQueueCreate(6, 20);  // creazione di Queue di 6 elementi di 20 byte ciascuno
 
- 
-
-
-  xQueueDatas = xQueueCreate(6, 20);
-
+  // creazione dei task radio
   xTaskCreatePinnedToCore(
-    radioTask, "radioTask"  // A name just for humans
+    radioTask, "radioTask"  // Nome del task
     ,
     1024 * 10  // This stack size can be checked & adjusted by reading the Stack Highwater
     ,
-    NULL, 200  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    NULL, 2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
     ,
     NULL, 1);
 
+  // creazione dei task upload
   xTaskCreatePinnedToCore(
     uploadTask, "uploadTask", 1024 * 64  // Stack size
     ,
-    NULL, 201 // Priority
+    NULL, 3  // Priority
     ,
     NULL, 0);
 }
 
-void loop() {
+void loop() {  // Non viene usato da RTOS
 }
 
 void radioTask(void *pvParameters) {
-
- 
-
   (void)pvParameters;
 
   for (;;) {
-    
-    if (radio.available()) {
+
+    if (radio.available())  //Quando è diasponibile un pacchetto
+    {
       Serial.print("Radio task running on core ");
-    Serial.println(xPortGetCoreID());
-      char data[20] = "";
-      radio.read(&data, sizeof(data));
+      Serial.println(xPortGetCoreID());
+      char data[20];
+      radio.read(&data, sizeof(data));  //riceve il dato nel formato valore-nome
       Serial.println(data);
-      xQueueSendToBack(xQueueDatas, &data, 0);
-
-
+      xQueueSendToBack(xQueueDatas, &data, 0);  // Inserisce il dato nella queue
+      vTaskDelay(10 / portTICK_PERIOD_MS);
     }
-vTaskDelay(1 / portTICK_PERIOD_MS);
+    vTaskDelay(1 / portTICK_PERIOD_MS);
   }
 }
 
 void uploadTask(void *pvParameters) {
 
   (void)pvParameters;
-  
-
-   
 
   WiFiClientSecure *client = new WiFiClientSecure;
   if (client) {
     client->setCACert(rootCACertificate);
 
-    HTTPClient https;
+    HTTPClient https;  //crea il client http
     https.setReuse(true);
 
-    if (https.begin(*client, api_server_url)) {
+    if (https.begin(*client, api_server_url))  //connessione permanente server delle API
+    {
       for (;;) {
         Serial.print("uploadTask running on core ");
         Serial.println(xPortGetCoreID());
-        int queue_items = uxQueueMessagesWaiting(xQueueDatas);
+        int queue_items = uxQueueMessagesWaiting(xQueueDatas);  //numero di elementi nella queue
 
-        if (queue_items > 0) {
+        if (queue_items > 0)  //se maggiore di 0 è arrivato un dato
+        {
           char data_string[20];
-          xQueueReceive(xQueueDatas, &data_string, 0);
+          xQueueReceive(xQueueDatas, &data_string, 0);  //prende il dato dalla queue
           String data_string_Str = String(data_string);
           receiveDataStruct data_parsed;
-          data_parsed = parseData(data_string_Str);
+          data_parsed = parseData(data_string_Str);  //conversione del dato nella struct
 
-          StaticJsonDocument<120> doc;              //creazione doc json per body  della richiesta http
-          String contentType = "application/json";  //formato body http
+          StaticJsonDocument<120> doc;              // creazione doc json per body  della richiesta http
+          String contentType = "application/json";  // formato body http
           String body;
           doc["key"] = key;  // aggiunta dati al doc json
           doc["datavalue"] = data_parsed.data_value;
-          serializeJson(doc, body);  //conversione del doc json in String
+          serializeJson(doc, body);  // conversione del doc json in String
           String req_path = "/v1/board/putdata/" + data_parsed.data_name;
           Serial.println(body);
           Serial.print("faccio una richiesta a: ");
           Serial.println(req_path);
 
-          https.setURL(req_path);
-          int httpCode = https.PUT(body);
+          https.setURL(req_path);          //import l'url sul nome del dato
+          int httpCode = https.PUT(body);  //invio dato al server
           Serial.println(httpCode);
-          vTaskDelay(100 / portTICK_PERIOD_MS);
+          vTaskDelay(50 / portTICK_PERIOD_MS);
         }
-        vTaskDelay(30 / portTICK_PERIOD_MS);
+        vTaskDelay(10 / portTICK_PERIOD_MS);
       }
     }
   }
-}  
+}
